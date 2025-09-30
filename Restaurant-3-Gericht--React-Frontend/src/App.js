9819494679ce974c479b63863d78a35b78309398
maanvi21 <maanvishadakshari21@gmail.com>
2025-09-30 16:39:47 +0530

    init

--- Restaurant-3-Gericht--React-Frontend/src/App.js
+++ Restaurant-3-Gericht--React-Frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { AboutUs, Chef, FindUs, Footer, Gallery, Intro, Laurels, SpecialMenu } from './container';
 import { Navbar } from './components';
 import './App.css';
 import Intro2 from './container/Intro/Intro2';
@@ -8,7 +8,6 @@ import Intro2 from './container/Intro/Intro2';
 const App = () => (
   <div>
     <Navbar />
-    <Header />
     <Intro2 />
     <AboutUs />
     <SpecialMenu />
--- Restaurant-3-Gericht--React-Frontend/src/container/Intro/Intro2.jsx
+++ Restaurant-3-Gericht--React-Frontend/src/container/Intro/Intro2.jsx
@@ -1,13 +1,26 @@
+/* eslint-disable quotes */
+/* eslint-disable no-trailing-spaces */
 /* eslint-disable eol-last */
 /* eslint-disable comma-dangle */
 /* eslint-disable no-lonely-if */
 import React from 'react';
+import { motion, useScroll, useTransform } from 'framer-motion';
 import './Intro.css';
 import { homevideo1 } from '../../constants';
+import { SubHeading } from '../../components';
 
 const Intro2 = () => {
   const vidRef = React.useRef();
   const containerRef = React.useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
+    offset: ["start end", "end start"]
+  });
+
+  const y = useTransform(scrollYProgress, [0, 1], [100, -100]);
+  const opacity = useTransform(scrollYProgress, [0, 0.2, 0.8, 1], [0, 1, 1, 0]);
+  const scale = useTransform(scrollYProgress, [0, 0.5, 1], [0.9, 1, 0.95]);
 
   React.useEffect(() => {
     const observer = new IntersectionObserver(
@@ -39,15 +52,71 @@ const Intro2 = () => {
   }, []);
 
   return (
-    <div className="app__video" ref={containerRef}>
-      <video
+    <div className="app__video-container" ref={containerRef}>
+      <motion.video
         ref={vidRef}
         src={homevideo1}
         type="video/mp4"
         loop
         controls={false}
         muted
+        className="app__video-background"
+        style={{ scale }}
       />
+      <motion.div 
+        className="app__video-overlay"
+        style={{ opacity }}
+      >
+        <div className="app__header app__wrapper section__padding" id="home">
+          <motion.div 
+            className="app__wrapper_info"
+            style={{ y }}
+          >
+            <motion.div
+              initial={{ opacity: 0, y: 30 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.8, delay: 0.2 }}
+              viewport={{ once: true }}
+            >
+              <SubHeading title="Chase the new flavour" />
+            </motion.div>
+            
+            <motion.h1 
+              className="app__header-h1"
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.8, delay: 0.4 }}
+              viewport={{ once: true }}
+            >
+              The Key To Fine Dining
+            </motion.h1>
+            
+            <motion.p 
+              className="p__opensans" 
+              style={{ margin: '2rem 0' }}
+              initial={{ opacity: 0, y: 30 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.8, delay: 0.6 }}
+              viewport={{ once: true }}
+            >
+              Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus
+            </motion.p>
+            
+            <motion.button 
+              type="button" 
+              className="custom__button"
+              initial={{ opacity: 0, y: 30 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.8, delay: 0.8 }}
+              viewport={{ once: true }}
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+            >
+              Explore Menu
+            </motion.button>
+          </motion.div>
+        </div>
+      </motion.div>
     </div>
   );
 };
